fdd8d78e3e2c4111d41db5836b2ab61c4c2f2d3c
rustly <43079937+Rustly@users.noreply.github.com>
2025-02-23 15:33:24 -0600

    Initial commit

---
+++ src/main/java/com/cen4802c/app/App.java
@@ -0,0 +1,22 @@
+package com.cen4802c.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        var value = 10;
+        var result = recursive_Fibonacci(value);
+        System.out.println("The Fibonacci sequence for " + value + " is " + result);
+    }
+
+
+    /**
+     * This static method calculates a Fibonacci sequence result via being a recursive function.
+     * @param numberToCheck The number to check the Fibonacci sequence with.
+     * @return The resulting calculation of the Fibonacci sequence.
+     */
+    private static int recursive_Fibonacci(int numberToCheck) {
+        return numberToCheck <= 1 ? numberToCheck : recursive_Fibonacci(numberToCheck - 1) + recursive_Fibonacci(numberToCheck - 2);
+    }
+}
---
+++ src/test/java/com/cen4802c/app/AppTest.java
@@ -0,0 +1,19 @@
+package com.cen4802c.app;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
